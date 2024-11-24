c890a590c5bb20af4bdff9bc0c1b73ffbbd9046a
DeepMind <noreply@google.com>
2024-11-24 10:28:58 -0800

    code for creating SimpleLogic
    PiperOrigin-RevId: 699728621
    Change-Id: I06a57f11d8468300a0b5d2e3f1b7a75db60a8b5c

---
+++ SimpleLogic/generate_ruleset.py
@@ -0,0 +1,117 @@
+# Copyright 2024 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Generates 1-sufficient sets for the SimpleLogic task."""
+
+import argparse
+import glob
+import json
+
+from questbench.SimpleLogic import derivation
+from questbench.SimpleLogic import holdout_utils
+from questbench.SimpleLogic import ruleset
+
+
+def main(arguments) -> None:
+  rules_dicts, files_to_rules_dicts = ruleset.load_data()
+  start_idx = int(arguments.start_idx)
+  end_idx = int(arguments.end_idx)
+  for item_file in files_to_rules_dicts:
+    write_file = item_file.replace(
+        ".txt", f"_{start_idx}_{end_idx}_heldout_fixed.jsonl"
+    )
+    heldout_files = item_file.replace(".txt", "*heldout_fixed.jsonl")
+    rules_dicts_existing = []
+    for existing_file in glob.glob(heldout_files):
+      with open(existing_file, "r") as f:
+        for line in f:
+          rules_dicts_existing.append(line)
+
+    existing_rules_dicts_rules_queries = set()
+    for rules_dict in rules_dicts_existing:
+      try:
+        rules_dict = json.loads(rules_dict)
+      except json.JSONDecodeError:
+        continue
+      existing_rules_dicts_rules_queries.add((
+          json.dumps(
+              ruleset.RuleTree.deserialize(rules_dict["rules"]).serialize()
+          ),
+          rules_dict["query"],
+      ))
+
+    for r, rules_dict in enumerate(rules_dicts):
+      if r < start_idx or r >= end_idx:
+        continue
+      rule_tree_rules = ruleset.RuleTree.deserialize(rules_dict["rules"])
+      if (
+          json.dumps(rule_tree_rules.serialize()),
+          rules_dict["query"],
+      ) in existing_rules_dicts_rules_queries:
+        continue
+      valid = derivation.get_derivations(rules_dict)
+      if not valid:
+        continue
+      holdout_utils.make_heldout_ruleset(rules_dict)
+      with open(write_file, "a") as wf:
+        wf.write(
+            json.dumps({
+                "rules": rule_tree_rules.serialize(),
+                "query": rules_dict["query"],
+                "depth": rules_dict["depth"],
+                "true_derivations": [
+                    derive.serialize()
+                    for derive in rules_dict["true_derivations"]
+                ],
+                "false_derivations": [
+                    derive.serialize()
+                    for derive in rules_dict["false_derivations"]
+                ],
+                "heldout_set_to_q": rules_dict["heldout_set_to_q"],
+                "heldout_set_to_subset_qs": rules_dict[
+                    "heldout_set_to_subset_qs"
+                ],
+            })
+            + "\n"
+        )
+        rules_dicts_existing.append(
+            json.dumps({
+                "rules": rule_tree_rules.serialize(),
+                "query": rules_dict["query"],
+                "depth": rules_dict["depth"],
+                "true_derivations": [
+                    derive.serialize()
+                    for derive in rules_dict["true_derivations"]
+                ],
+                "false_derivations": [
+                    derive.serialize()
+                    for derive in rules_dict["false_derivations"]
+                ],
+                "heldout_set_to_q": rules_dict["heldout_set_to_q"],
+                "heldout_set_to_subset_qs": rules_dict[
+                    "heldout_set_to_subset_qs"
+                ],
+            })
+            + "\n"
+        )
+        print(f"Wrote {r}/{len(rules_dicts)} to " + write_file)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--start_idx", type=int, default=0)
+  parser.add_argument("--end_idx", type=int)
+  args = parser.parse_args()
+  main(args)
---
+++ SimpleLogic/holdout_utils.py
@@ -0,0 +1,128 @@
+# Copyright 2024 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Utilities for generating ambiguous questions by holding out 1 word."""
+
+import itertools as it
+import json
+
+import tqdm
+
+from questbench.SimpleLogic import derivation
+from questbench.SimpleLogic import ruleset
+
+
+def make_heldout_ruleset(rules_dict):
+  """Hold out 1 word required for deriving the target word's truth value.
+
+  Keeps only those derivations that are not already derived by the full set.
+  Adds field to rules_dict called `heldout_set_to_q` and
+  `heldout_set_to_subset_qs`
+  `heldout_set_to_q` maps a heldout set to a dict of the form
+  {1-sufficient set: sufficient word: {true_derivation, false_derivation}}
+  where 1-sufficient set is missing 1 word that is required for deriving the
+  target word's truth value, and sufficient word is a word that when known, can
+  derive the target word's truth value. The true/false derivations are the
+  derivations of the target word's truth value after adding the sufficient word
+  to the heldout set.
+  `heldout_set_to_subset_qs` maps a 1-sufficient set to a list of heldout sets
+  that are subsets of the heldout set, but not the heldout set itself.
+
+  Args:
+    rules_dict: Dict[str, Any]
+  """
+  true_derivations = {}
+  for derive in rules_dict['true_derivations']:
+    if not isinstance(derive, derivation.ConjunctionRule):
+      derive = derivation.ConjunctionRule.deserialize(derive)
+    true_derivations[frozenset(derive.leaf_words.keys())] = derive
+  false_derivations = {}
+  for derive in rules_dict['false_derivations']:
+    if not isinstance(derive, derivation.ConjunctionRule):
+      derive = derivation.ConjunctionRule.deserialize(derive)
+    false_derivations[frozenset(derive.leaf_words.keys())] = derive
+
+  heldout_set_to_q_depth_expansions = {}
+  # get combined version (cross product)
+  for true_derive, false_derive in tqdm.tqdm(
+      it.product(true_derivations, false_derivations),
+      total=len(true_derivations) * len(false_derivations),
+  ):
+    # find pairs that differ on exactly 1 var
+    differ_word = None
+    for k in true_derive:
+      if ruleset.negate(k) in false_derive:
+        if differ_word is None:
+          differ_word = k
+        elif differ_word != k:
+          differ_word = None
+          break
+    if differ_word is None:
+      continue
+
+    heldout_set = true_derive.union(false_derive)
+    heldout_set -= {differ_word, ruleset.negate(differ_word)}
+
+    # can already derive heldout set
+    skip_this = False
+    if heldout_set in true_derivations or heldout_set in false_derivations:
+      skip_this = True
+    else:
+      for true_derivation in true_derivations:
+        if true_derivation <= heldout_set:
+          skip_this = True
+          break
+      for false_derivation in false_derivations:
+        if false_derivation <= heldout_set:
+          skip_this = True
+          break
+    if skip_this:
+      continue
+
+    if heldout_set not in heldout_set_to_q_depth_expansions:
+      heldout_set_to_q_depth_expansions[heldout_set] = {}
+    # add variable with direction that implies query word is true
+    heldout_set_to_q_depth_expansions[heldout_set][differ_word] = {
+        'true_derivation': true_derivations[true_derive].serialize(),
+        'false_derivation': false_derivations[false_derive].serialize(),
+    }
+
+  rules_dict['heldout_set_to_q'] = {
+      json.dumps(list(heldout_set)): heldout_set_to_q_depth_expansions[
+          heldout_set
+      ]
+      for heldout_set in heldout_set_to_q_depth_expansions
+  }
+
+  # valid questions of subset --> also valid questions here,
+  # but we want to avoid asking
+  heldout_set_to_subset_qs = {}
+  for heldout_set in heldout_set_to_q_depth_expansions:
+    heldout_set_to_subset_qs[heldout_set] = set()
+    for other_set in heldout_set_to_q_depth_expansions:
+      if heldout_set == other_set:
+        continue
+      # check if other_set is a subset of heldout_Set
+      if other_set < heldout_set:
+        heldout_set_to_subset_qs[heldout_set] = heldout_set_to_subset_qs[
+            heldout_set
+        ].union(set(heldout_set_to_q_depth_expansions[other_set].keys()))
+      # remove target_attr from set
+      if rules_dict['query'] in heldout_set_to_subset_qs[heldout_set]:
+        heldout_set_to_subset_qs[heldout_set].remove(rules_dict['query'])
+  rules_dict['heldout_set_to_subset_qs'] = {
+      json.dumps(list(heldout_set)): list(heldout_set_to_subset_qs[heldout_set])
+      for heldout_set in heldout_set_to_subset_qs
+  }
